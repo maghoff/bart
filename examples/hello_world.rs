741e7e5a0ca7ca49f7cd0a654f239766e0a6184c
Magnus Hoff <maghoff@gmail.com>
2017-01-07 12:39:39 +0100

    Add an example

---
+++ examples/hello_world.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate bart_derive;
+
+#[derive(BartDisplay)]
+#[template = "examples/hello_world.html"]
+struct HelloWorld<'a> {
+    name: &'a str,
+}
+
+fn main() {
+    print!("{}", &HelloWorld { name: "World" });
+}
