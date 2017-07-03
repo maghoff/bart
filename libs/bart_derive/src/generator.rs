591e7227090e8595c51e39aa37db195ea5d6d6b8
Magnus Hoff <maghoff@gmail.com>
2017-07-03 15:25:23 +0200

    Refactor generator code

--- libs/bart_derive/src/generator.rs
+++ libs/bart_derive/src/generator.rs
@@ -3,7 +3,7 @@ use syn;
 use token;
 use quote;
 
-fn resolve(name: &token::Name, scope_depth: u32) -> String {
+fn resolve(name: &token::Name, scope_depth: u32) -> syn::Ident {
     let root = match name.leading_dots {
         0 => "_s0".to_owned(),
         x => format!("_s{}", scope_depth.checked_sub(x).expect("Too many dots")),
@@ -15,7 +15,15 @@ fn resolve(name: &token::Name, scope_depth: u32) -> String {
         buf.push_str(segment);
     }
 
-    buf
+    syn::Ident::new(buf)
+}
+
+fn scope<'a>(name: token::Name, scope_level: u32, ast: ast::Ast<'a>) -> (syn::Ident, syn::Ident, quote::Tokens) {
+    let name = resolve(&name, scope_level);
+    let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
+    let nested_generated = generate(ast, scope_level + 1);
+
+    (name, scope_variable, nested_generated)
 }
 
 pub fn generate(node: ast::Ast, scope_level: u32) -> quote::Tokens {
@@ -29,63 +37,53 @@ pub fn generate(node: ast::Ast, scope_level: u32) -> quote::Tokens {
             quote! { f.write_str(#text)?; }
         },
         Interpolation(name) => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
+            let name = resolve(&name, scope_level);
             quote! { _bart::DisplayHtmlSafe::safe_fmt(&#name, f)?; }
         },
         UnescapedInterpolation(name) => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
+            let name = resolve(&name, scope_level);
             quote! { ::std::fmt::Display::fmt(&#name, f)?; }
         },
         Iteration { name, nested } => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
-            let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
-            let nested_generated = generate(*nested, scope_level + 1);
+            let (name, scope_variable, nested) = scope(name, scope_level, *nested);
             quote! {
                 for ref #scope_variable in (&#name).into_iter() {
-                    #nested_generated
+                    #nested
                 }
             }
         },
         NegativeIteration { name, nested } => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
-            let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
-            let nested_generated = generate(*nested, scope_level + 1);
+            let (name, scope_variable, nested) = scope(name, scope_level, *nested);
             quote! {
                 for ref #scope_variable in _bart::NegativeIterator::neg_iter(&#name) {
-                    #nested_generated
+                    #nested
                 }
             }
         },
         Conditional { name, nested } => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
-            let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
-            let nested_generated = generate(*nested, scope_level + 1);
+            let (name, scope_variable, nested) = scope(name, scope_level, *nested);
             quote! {
                 if _bart::Conditional::val(&#name) {
                     let ref #scope_variable = #name;
-                    #nested_generated
+                    #nested
                 }
             }
         },
         NegativeConditional { name, nested } => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
-            let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
-            let nested_generated = generate(*nested, scope_level + 1);
+            let (name, scope_variable, nested) = scope(name, scope_level, *nested);
             quote! {
                 if !_bart::Conditional::val(&#name) {
                     let ref #scope_variable = #name;
-                    #nested_generated
+                    #nested
                 }
             }
         },
         Scope { name, nested } => {
-            let name = syn::Ident::new(resolve(&name, scope_level));
-            let scope_variable = syn::Ident::new(format!("_s{}", scope_level));
-            let nested_generated = generate(*nested, scope_level + 1);
+            let (name, scope_variable, nested) = scope(name, scope_level, *nested);
             quote! {
                 {
                     let ref #scope_variable = #name;
-                    #nested_generated
+                    #nested
                 }
             }
         },
